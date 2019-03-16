dc1756fe9bec6a478a938276be6a450021b67b82
Daniel de Waard <danieldewaard@users.noreply.github.com>
2019-03-17 01:52:46 +1100

    Javascript file for the UI.
    function to handle the buttons changing colour depending on what has been pressed.

---
+++ ui.js
@@ -0,0 +1,81 @@
+var toggle = 0;
+var buttonStatus = [];
+
+//code for changing the colour of the buttons after they have been pressed. 
+function buttonStyleHandler(_this) {
+
+//get all the button elements and their details    
+const list = document .getElementsByClassName("btn-secondary");   
+    //switch the current status of the button that was pressed. 
+  _this.activated = !_this.activated;
+    
+  //if the button that was pressed was inactive but is now active....    
+  if ( _this.activated == 1) {
+   
+    if(_this.id == "button1"){  
+        _this.style.backgroundColor = "#29d148";
+    }
+    else{
+        _this.style.backgroundColor ="#339966";
+    }  
+    _this.style.color = "white";
+  }
+  
+  //if the button is now inactive  
+  else {
+      
+    _this.style.backgroundColor = "buttonface";
+    _this.style.color = "black";
+      
+    if (buttonStatus[0] == true){
+        
+        list[0].activated = false;
+        list[0].style.backgroundColor = "buttonface";
+        list[0].style.color = "black";
+        
+    }    
+      
+  }
+    
+  //handle the button All being pressed   
+  if (_this.id == "button1"){ 
+    
+      var i;
+      
+      for(i=0;i<list.length;i++){
+        
+        if(list[i].id == "button1"){
+            
+        }
+        else{  
+        list[i].activated = _this.activated; 
+            
+            //turn on all buttons 
+            if (list[i].activated == 1){
+            
+                list[i].style.backgroundColor = "#339966";
+                list[i].style.color = "white";
+                
+            }
+            
+            //turn off all buttons if all button is turned off. 
+            else {
+                
+                list[i].style.backgroundColor = "buttonface";
+                list[i].style.color = "black";
+                
+            }
+        }
+    }  
+  }
+    
+    //update the status of all buttons. 
+    var j;
+    for(j=0;j<list.length;j++){
+
+        buttonStatus[j] = list[j].activated;
+
+        }
+
+    console.log(buttonStatus);    
+}
