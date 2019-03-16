59e0f2e512e788d20cbcf56bfd86fc86dfc34718
NehaWAU <30915155+NehaWAU@users.noreply.github.com>
2019-03-16 23:04:53 +1100

    Updated version of map

--- my-mapbox.js
+++ my-mapbox.js
@@ -105,7 +105,9 @@ function mapLoaded() {
         }
     }, labelLayerId);
 
+
 // ------------------------ Hospital Layers
+
     map.addLayer({
         "id": "points",
         "type": "symbol",
@@ -132,43 +134,46 @@ function mapLoaded() {
 
 
     map.addLayer({
-        "id": "earthquakes-heat",
+        "id": "hospitals-heat",
         "type": "heatmap",
         "source": "Hospitals",
         "maxzoom": 9,
         "paint": {
-// Increase the heatmap weight based on frequency and property magnitude
-            "heatmap-weight": [
-                "interpolate",
-                ["linear"],
-                ["get", "mag"],
-                0, 0,
-                6, 1
-            ],
-// Increase the heatmap color weight weight by zoom level
-// heatmap-intensity is a multiplier on top of heatmap-weight
-            "heatmap-intensity": [
+        // Increase the heatmap weight based on frequency and property magnitude
+            "heatmap-weight": 1
+            //     [
+            //     "interpolate",
+            //     ["linear"],
+            //     ["get", "mag"],
+            //     0, 0,
+            //     6, 1
+            // ]
+            ,
+        // Increase the heatmap color weight weight by zoom level
+        // heatmap-intensity is a multiplier on top of heatmap-weight
+            "heatmap-intensity":
+                [
                 "interpolate",
                 ["linear"],
                 ["zoom"],
-                1, 10,
-                9, 3
-            ],
-// Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
-// Begin color ramp at 0-stop with a 0-transparancy color
-// to create a blur-like effect.
-            "heatmap-color": [
-                "interpolate",
+                0, 10,
+                9, 2
+            ]
+            ,
+        // Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
+        // Begin color ramp at 0-stop with a 0-transparancy color
+        // to create a blur-like effect.
+            "heatmap-color":
+                ["interpolate",
                 ["linear"],
                 ["heatmap-density"],
-                0, "rgba(33,102,172,0)",
-                0.2, "rgb(103,169,207)",
-                0.4, "rgb(209,229,240)",
-                0.6, "rgb(253,219,199)",
-                0.8, "rgb(239,138,98)",
-                1, "rgb(178,24,43)"
-            ],
-// Adjust the heatmap radius by zoom level
+                    0, "rgba(100,102,172,0)",
+                    0.3, "rgb(50,0,255)",
+                    0.5, "rgb(50,50,150)",
+                    0.8, "rgb(150,50,50)",
+                    1, "rgb(255,50,50)"]
+            ,
+        // Adjust the heatmap radius by zoom level
             "heatmap-radius": [
                 "interpolate",
                 ["linear"],
@@ -176,7 +181,7 @@ function mapLoaded() {
                 2, 10,
                 5, 50
             ],
-// Transition from heatmap to circle layer by zoom level
+        // Transition from heatmap to circle layer by zoom level
             "heatmap-opacity": [
                 "interpolate",
                 ["linear"],
@@ -193,7 +198,7 @@ function mapLoaded() {
         "source": "Hospitals",
         "minzoom": 7,
         "paint": {
-// Size circle radius by earthquake magnitude and zoom level
+            // Size circle radius by earthquake magnitude and zoom level
             "circle-radius": [
                 "interpolate",
                 ["linear"],
@@ -213,21 +218,11 @@ function mapLoaded() {
                     6, 50
                 ]
             ],
-// Color circle by earthquake magnitude
-            "circle-color": [
-                "interpolate",
-                ["linear"],
-                ["get", "mag"],
-                1, "rgba(33,102,172,0)",
-                2, "rgb(103,169,207)",
-                3, "rgb(209,229,240)",
-                4, "rgb(253,219,199)",
-                5, "rgb(239,138,98)",
-                6, "rgb(178,24,43)"
-            ],
+        // Color circle by earthquake magnitude
+            "circle-color": "rgb(0, 0, 255)",
             "circle-stroke-color": "white",
             "circle-stroke-width": 1,
-// Transition from heatmap to circle layer by zoom level
+        // Transition from heatmap to circle layer by zoom level
             "circle-opacity": [
                 "interpolate",
                 ["linear"],
@@ -238,11 +233,10 @@ function mapLoaded() {
         }
     }, 'waterway-label');
 
-
-
-    //----------------------------- Schools Layers
+/*
+//----------------------------- Schools Layers
     map.addLayer({
-        "id": "points",
+        "id": "schoolsPoints",
         "type": "symbol",
         "source":
             {
@@ -259,15 +253,13 @@ function mapLoaded() {
             }
     });
 
-
-    map.addSource('Hospitals', {
+    map.addSource('Schools', {
         "type": "geojson",
         "data": SchoolsData
     });
 
-
     map.addLayer({
-        "id": "earthquakes-heat",
+        "id": "schools-heat",
         "type": "heatmap",
         "source": "Schools",
         "maxzoom": 9,
@@ -323,7 +315,7 @@ function mapLoaded() {
     }, 'waterway-label');
 
     map.addLayer({
-        "id": "earthquakes-point",
+        "id": "schools-point",
         "type": "circle",
         "source": "Schools",
         "minzoom": 7,
@@ -345,21 +337,11 @@ function mapLoaded() {
                     ["linear"],
                     ["get", "mag"],
                     1, 5,
-                    6, 50
+                    6, 20
                 ]
             ],
 // Color circle by earthquake magnitude
-            "circle-color": [
-                "interpolate",
-                ["linear"],
-                ["get", "mag"],
-                1, "rgba(33,102,172,0)",
-                2, "rgb(103,169,207)",
-                3, "rgb(209,229,240)",
-                4, "rgb(253,219,199)",
-                5, "rgb(239,138,98)",
-                6, "rgb(178,24,43)"
-            ],
+            "circle-color": "rgb(255, 0, 0)",
             "circle-stroke-color": "white",
             "circle-stroke-width": 1,
 // Transition from heatmap to circle layer by zoom level
@@ -372,6 +354,6 @@ function mapLoaded() {
             ]
         }
     }, 'waterway-label');
-
+*/
 
 }
