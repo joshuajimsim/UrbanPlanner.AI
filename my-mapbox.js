158dfc55277701fa7cf735623ed092aae4336d9e
NehaWAU <30915155+NehaWAU@users.noreply.github.com>
2019-03-17 11:36:27 +1100

    Update my-mapbox.js

--- my-mapbox.js
+++ my-mapbox.js
@@ -383,6 +383,74 @@ function mapLoaded() {
         map.getCanvas().style.cursor = '';
     });
 
+    
+    
+//------hospitals label name
+    // When a click event occurs on a feature in the places layer, open a popup at the
+// location of the feature, with label name HTML from its properties.
+    map.on('click', 'hospitals-point', function (e) {
+        var coordinates = e.features[0].geometry.coordinates.slice();
+        var description = e.features[0].properties.LabelName;
+
+// Ensure that if the map is zoomed out such that multiple
+// copies of the feature are visible, the popup appears
+// over the copy being pointed to.
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+
+        new mapboxgl.Popup()
+            .setLngLat(coordinates)
+            .setHTML(description)
+            .addTo(map);
+    });
+
+// Change the cursor to a pointer when the mouse is over the places layer.
+    map.on('mouseenter', 'hospitals-point', function () {
+        map.getCanvas().style.cursor = 'pointer';
+    });
+
+// Change it back to a pointer when it leaves.
+    map.on('mouseleave', 'hospitals-point', function () {
+        map.getCanvas().style.cursor = '';
+    });
+
+//-------schools names
+    // When a click event occurs on a feature in the places layer, open a popup at the
+// location of the feature, with label name HTML from its properties.
+    map.on('click', 'schools-point', function (e) {
+        var coordinates = e.features[0].geometry.coordinates.slice();
+        var description = e.features[0].properties.School_Name;
+
+// Ensure that if the map is zoomed out such that multiple
+// copies of the feature are visible, the popup appears
+// over the copy being pointed to.
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+
+        new mapboxgl.Popup()
+            .setLngLat(coordinates)
+            .setHTML(description)
+            .addTo(map);
+    });
+
+// Change the cursor to a pointer when the mouse is over the places layer.
+    map.on('mouseenter', 'schools-point', function () {
+        map.getCanvas().style.cursor = 'pointer';
+    });
+
+// Change it back to a pointer when it leaves.
+    map.on('mouseleave', 'schools-point', function () {
+        map.getCanvas().style.cursor = '';
+    });
+
+
+    
+    
+    
+    
+    
 //---- recommended points
     map.addLayer({
         "id": "recommendation_points",
